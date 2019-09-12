e64c714d7347d6fd1cb1fcc2d6f0bc9092d51ed3
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-12 00:28:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,7 @@ def long_name_steals_a_ton?
     most_steals = steals.max
    end
    
-   player = team_details[:players][:player_name].find {|x| x == long_name}
+   player = team_details[:players].find {|x| x == long_name}
      puts player
       #&& player[:steals] == most_steals
     #  p player[:player_name]
