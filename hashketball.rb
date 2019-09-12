dc2298827b0255f8da94103ff3c5cd1eab0c7da8
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-12 00:26:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,7 +177,7 @@ def long_name_steals_a_ton?
    end
    
    if team_details[:players][:player_name] == [long_name] 
-     p team_details[:players][:player_name][long_name]
+     p "up"
       #&& player[:steals] == most_steals
     #  p player[:player_name]
      # p player[:steals]
